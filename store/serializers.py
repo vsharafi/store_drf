0b339ffcf1515a0dca9b8268085434fc4be72880
vahid <vahid.sh1431@yahoo.com>
2024-06-14 02:08:58 +0330

    add post method for product list

--- store/serializers.py
+++ store/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 from decimal import Decimal
+from django.utils.text import slugify
 
-from store.models import Category
+from store.models import Category, Product
 
 
 class CategorySerializer(serializers.Serializer):
@@ -9,18 +10,28 @@ class CategorySerializer(serializers.Serializer):
     description = serializers.CharField(max_length=500)
 
 
-class ProductSerializer(serializers.Serializer):
+class ProductSerializer(serializers.ModelSerializer):
     TAX = 0.09
 
-    id = serializers.IntegerField()
-    name = serializers.CharField(max_length=255)
-    unit_price = serializers.DecimalField(max_digits=6, decimal_places=2)
-    inventory = serializers.IntegerField()
+    title = serializers.CharField(max_length=255, source='name')
+    price = serializers.DecimalField(max_digits=6, decimal_places=2, source="unit_price")
     price_after_tax = serializers.SerializerMethodField()
-    category = serializers.HyperlinkedRelatedField(
-        queryset=Category.objects.all(),
-        view_name="category_detail",
-    )
+
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'price', 'category', 'price_after_tax', 'inventory', 'slug', 'description']
+    
 
     def get_price_after_tax(self, product):
         return round(product.unit_price * Decimal(1 + self.TAX), 2)
+    
+    def validate(self, data):
+        if len(data['name']) < 6:
+            raise serializers.ValidationError("Product title should be at least 6.")
+        return super().validate(data)
+    
+    def create(self, validated_data):
+        product = Product(**validated_data)
+        product.slug = slugify(product.name)
+        product.save()
+        return product
--- store/views.py
+++ store/views.py
@@ -1,15 +1,22 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from django.shortcuts import get_object_or_404
 
 from .models import Category, Product
 from .serializers import CategorySerializer, ProductSerializer
 
 
-@api_view()
+@api_view(['GET', 'POST'])
 def product_list(request):
-    products_queryset = Product.objects.select_related('category')
-    serializer = ProductSerializer(products_queryset, many=True, context={'request': request})
+    if request.method == "GET":
+        products_queryset = Product.objects.select_related('category')
+        serializer = ProductSerializer(products_queryset, many=True, context={'request': request})
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response('OK')
     return Response(serializer.data)
 
 
@@ -18,7 +25,8 @@ def product_detail(request, pk):
     product = get_object_or_404(Product.objects.select_related('category'), pk=pk)
     serializer = ProductSerializer(product, context={'request': request})
     return Response(serializer.data)
-
+    
+    
 
 @api_view()
 def category_detail(request, pk):
