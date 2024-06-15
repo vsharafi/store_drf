6365c475da87501294b3ce2fac922088851ef3b8
vahid <vahid.sh1431@yahoo.com>
2024-06-15 21:45:17 +0330

    add admin for custom user model

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", 'email', "password1", "password2", 'first_name', 'last_name'),
+            },
+        ),
+    )
